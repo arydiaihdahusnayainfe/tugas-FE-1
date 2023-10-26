8d515db2a722635696bc66aae5db981c6292da13
arydiaihdahusnayainfe <arydiaihdahusnayainfe@gmail.com>
2023-10-26 12:38:15 +0700

    fixing Footer

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     
     return(
-        <footer className='absolute bottom-4'>
+        <footer className='absolute bottom-4 right-0 left-0'>
             <p className='text-slate-600 text-mg'>Created by @Pulupulu</p>
             <p className='text-slate-600 text-mg'>Belajar Frontend, Backend, dan UI Design sambil mengaji di <a className='hover:text-slate-900' href="http://qodr.id">https://qodr.id</a></p>
         </footer>
